dfceffa7cf9ab0ae5e75a30d0e336070e5707142
wanzenried <wanzenried@users.noreply.github.com>
2020-04-02 12:11:44 +0200

    Create player.js

---
+++ player.js
@@ -0,0 +1,65 @@
+class player {
+  constructor(location,w,h,speed,powerUpState = "smallMario") {
+    this.location = location;
+    this.width = w;
+    this.height = h;
+    this.powerUpState = powerUpState;
+    this.acceleration = createVector(0,0);
+    this.velocity = createVector(0,0);
+    this.speed = speed
+    this.onGround = false;
+  }
+
+  update() {
+    this.acceleration.add(gravity);
+
+    if(keyIsDown(68)){ // d key
+      this.acceleration.x += this.speed
+    }
+    if (keyIsDown(65)) { // a key
+      this.acceleration.x -= this.speed
+    }
+    // if (keyIsDown(32) && this.onGround) {
+    //   this.acceleration.y -= 20;
+    //   this.onGround = false;
+    // }
+    this.velocity.add(this.acceleration);
+
+
+
+
+    this.acceleration.mult(0);
+
+
+    let friction = this.velocity.copy()
+    friction.mult(-1);
+    friction.normalize();
+    friction.mult(0.1);
+    this.velocity.add(friction);
+    this.location.add(this.velocity)
+    if(this.velocity.x < 0.0001 && this.velocity.x > -0.0001) this.velocity.x = 0
+    console.log(this.velocity.x);
+
+    if(this.location.y > height-this.height){
+      this.location.y = height-this.height;
+      this.velocity.y = 0;
+      this.onGround = true;
+    }
+
+
+    return this;
+  }
+
+  jump(){
+    if (this.onGround) {
+      this.acceleration.y -= 20;
+      this.onGround = false;
+    }
+  }
+
+  draw() {
+    fill(255,0,0);
+    rect(this.location.x,this.location.y,this.width,this.height);
+  }
+
+}
