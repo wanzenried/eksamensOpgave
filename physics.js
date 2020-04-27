b98abc2fe4b061ae34246579ecbd9738af6200df
Richard <62063187+wanzenried@users.noreply.github.com>
2020-04-27 13:46:43 +0200

    Update physics.js

--- physics.js
+++ physics.js
@@ -35,7 +35,7 @@ class PhysicsObject {
       goToY: 0
     };
     if (!object.collide)
-    return returnParams
+      return returnParams
 
     //first check if the two rectangular hitboxes touch:
 
@@ -61,14 +61,14 @@ class PhysicsObject {
 
 
     //top detection
-    if (angle >= -(3*QUARTER_PI) && angle <= -QUARTER_PI) {
+    if (angle >= -(3 * QUARTER_PI) && angle <= -QUARTER_PI) {
       returnParams.top = true;
       returnParams.goToX = l1.x;
       returnParams.goToY = l3.y - this.height;
       return returnParams;
     }
     //left detection
-    else if ((angle > (3*QUARTER_PI) && angle < PI)||(angle > -(PI) &&angle < -(3*QUARTER_PI))) {
+    else if ((angle > (3 * QUARTER_PI) && angle < PI) || (angle > -(PI) && angle < -(3 * QUARTER_PI))) {
       returnParams.left = true;
       returnParams.goToX = l3.x - this.width;
       returnParams.goToY = l1.y;
@@ -82,18 +82,105 @@ class PhysicsObject {
       return returnParams;
     }
     //bottom Detection
-    else if (angle >= QUARTER_PI && angle <= (3*QUARTER_PI)) {
+    else if (angle >= QUARTER_PI && angle <= (3 * QUARTER_PI)) {
       returnParams.bottom = true;
       returnParams.goToX = l1.x;
       returnParams.goToY = r3.y;
       return returnParams;
-    }
-
-    else {
+    } else {
       returnParams.collision = false;
       return returnParams;
     }
-}
+  }
+
+  tallCollision(object) {
+    //cornerpoints of the calling objects hitbox
+    let l1 = createVector(this.location.x, this.location.y);
+    let r1 = createVector(l1.x + this.width, l1.y + this.height);
+    //cornerpoints of the calling objects hitbox last frame
+    let l2 = createVector(this.lastLocation.x, this.lastLocation.y);
+    let r2 = createVector(l2.x + this.width, l2.y + this.height);
+    //cornerpoints of the object being checkeds hitbox
+    let l3 = createVector(object.location.x, object.location.y);
+    let r3 = createVector(l3.x + object.width, l3.y + object.height);
+
+    //create return parameters:
+    let returnParams = {
+      collision: false,
+      top: false,
+      bottom: false,
+      left: false,
+      right: false,
+      goToX: 0,
+      goToY: 0
+    };
+    if (!object.collide)
+      return returnParams
+
+    //first check if the two rectangular hitboxes touch:
+
+    //if one of the rectangles is on the left of the other rectangles, they do not touch
+    if (l1.x > r3.x || l3.x > r1.x) {
+      returnParams.collision = false;
+      return returnParams;
+    }
+    //if one of the rectangles is above the other rectangle, they do not touch
+    if (l1.y > r3.y || l3.y > r1.y) {
+      returnParams.collision = false;
+      return returnParams;
+    }
+
+    returnParams.collision = true;
+
+    let p = [{
+      top: false,
+      bottom: false,
+      left: false,
+      right: false
+    }, {
+      {
+        top: false,
+        bottom: false,
+        left: false,
+        right: false
+      }
+    }];
+
+    let pos = [l2.copy(),l2.copy()];
+    pos[1].x -= unit;
+
+    for (var i = 0; i < 2; i++) {
+      let f = l2.copy();
+
+      f.sub(l3);
+
+
+      let angle = f.heading();
+
+
+      //top detection
+      if (angle >= -(3 * QUARTER_PI) && angle <= -QUARTER_PI) {
+        p[i].top = true;
+        break;
+      }
+      //left detection
+      else if ((angle > (3 * QUARTER_PI) && angle < PI) || (angle > -(PI) && angle < -(3 * QUARTER_PI))) {
+        p[i].left = true;
+        break;
+      }
+      //right Detection
+      else if (angle > -QUARTER_PI && angle < QUARTER_PI) {
+        p[i].right = true;
+        break;
+      }
+      //bottom Detection
+      else if (angle >= QUARTER_PI && angle <= (3 * QUARTER_PI)) {
+        p[i].bottom = true;
+        break;
+      }
+    }
+
+  }
 
   addForce(force) {
     this.acceleration.add(force);
